3bd7fcf3dcf77556dd19f67f024c947fd6b3bc55
Martin Bean <martin@mcbwebdesign.co.uk>
2013-07-10 17:30:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# External Link Warning
+
+Just a simple jQuery plugin to give users (with JavaScript enabled) a warning before redirecting to an external website.
+
+## License
+
+This plugin was created by [Martin Bean](http://www.martinbean.co.uk/) and is licensed under the MIT License.
---
+++ jquery.externalLinkWarning.js
@@ -0,0 +1,22 @@
+;(function($) {
+    
+    $.fn.externalLinkWarning = function() {
+        $('a').on('click', function(e) {
+            // create temporary anchor
+            var tmpLink = document.createElement('a');
+            tmpLink.href = this.href;
+            
+            // check if anchor host matches top window host
+            var externalLink = (tmpLink.host !== top.location.host);
+            
+            // if this is an external link, show confirmation dialog
+            if (externalLink) {
+                return confirm('This link will take you to an external website. Proceed with caution.');
+            }
+            
+            // if we're here just return true to allow propagation as normal
+            return true;
+        });
+    };
+    
+})(jQuery);
